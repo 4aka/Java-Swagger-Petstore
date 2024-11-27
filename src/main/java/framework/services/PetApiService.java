28b98eab2b35b19c6d5f63f1f3967492e5b96410
4aka <7nuclear@gmail.com>
2024-11-27 16:22:31 +0200

    Add GET method for /findByStatus

--- src/main/java/framework/services/PetApiService.java
+++ src/main/java/framework/services/PetApiService.java
@@ -1,17 +1,23 @@
 package framework.services;
 
-
 import models.pet.requestModel.PetCreateRequest;
 import framework.assertions.AssertableResponse;
-import io.qameta.allure.Step;
 
 public class PetApiService extends ApiService {
 
-    @Step
     public AssertableResponse addPet(PetCreateRequest petModel) {
         return new AssertableResponse(setUp()
+                .spec(REQUEST_SPEC)
                 .body(petModel)
                 .when()
                 .post("pet"));
+    }
+
+    public AssertableResponse getFindByStatus(String status) {
+        return new AssertableResponse(setUp()
+                .spec(REQUEST_SPEC)
+                .queryParam("status", status)
+                .when()
+                .get("/pet/findByStatus"));
     }
 }
