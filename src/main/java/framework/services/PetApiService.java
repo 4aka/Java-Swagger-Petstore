34ddd34f615428e92b82289beea412660ae6432b
4aka <7nuclear@gmail.com>
2024-11-27 21:03:55 +0200

    Add GET, PUT methods

--- src/main/java/framework/services/PetApiService.java
+++ src/main/java/framework/services/PetApiService.java
@@ -1,7 +1,9 @@
 package framework.services;
 
-import models.pet.requestModel.PetCreateRequest;
 import framework.assertions.AssertableResponse;
+import framework.enums.PetStatus;
+import models.pet.requestModel.PetCreateRequest;
+import models.pet.requestModel.PetEditRequest;
 
 public class PetApiService extends ApiService {
 
@@ -13,11 +15,27 @@ public class PetApiService extends ApiService {
                 .post("pet"));
     }
 
-    public AssertableResponse getFindByStatus(String status) {
+    public AssertableResponse getFindByStatus(PetStatus status) {
         return new AssertableResponse(setUp()
                 .spec(REQUEST_SPEC)
-                .queryParam("status", status)
+                .queryParam("status", status.getStatus())
                 .when()
                 .get("/pet/findByStatus"));
+    }
+
+    public AssertableResponse getPetById(long petId) {
+        return new AssertableResponse(setUp()
+                .spec(REQUEST_SPEC)
+                .queryParam("petId", petId)
+                .when()
+                .get("/pet/" + petId));
+    }
+
+    public AssertableResponse EditPetById(PetEditRequest petRequest) {
+         return new AssertableResponse(setUp()
+                .spec(REQUEST_SPEC)
+                .body(petRequest)
+                .when()
+                .put("/pet/"));
     }
 }
