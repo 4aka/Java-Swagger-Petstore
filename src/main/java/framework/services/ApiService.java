a028698ed4193534b093d3855894115a51dfa87d
4aka <7nuclear@gmail.com>
2024-11-27 16:19:04 +0200

    Update ApiService with prebuild methods to hide request realisation

--- src/main/java/framework/services/ApiService.java
+++ src/main/java/framework/services/ApiService.java
@@ -3,17 +3,34 @@ package framework.services;
 import framework.Config;
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.Filter;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.BeforeSuite;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class ApiService {
+
+    public static final RequestSpecification REQUEST_SPEC =
+            new RequestSpecBuilder()
+                    .setContentType("application/json")
+                    .setBaseUri(Config.restApiBaseUrl)
+                    .build();
+
+    @BeforeSuite
+    public void addFilters() {
+        RestAssured.filters(
+                new AllureRestAssured(),
+                new RequestLoggingFilter(),
+                new ResponseLoggingFilter()
+        );
+    }
 
     protected RequestSpecification setUp() {
         return RestAssured
