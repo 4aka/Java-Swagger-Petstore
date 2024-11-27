949df11654a75fc7e89bba6a4c5df1b343cfbdc8
4aka <7nuclear@gmail.com>
2024-11-27 16:23:49 +0200

    Add wrong status

--- src/main/java/framework/enums/PetStatus.java
+++ src/main/java/framework/enums/PetStatus.java
@@ -7,7 +7,8 @@ public enum PetStatus {
 
     AVAILABLE("available"),
     PENDING("pending"),
-    SOLD("sold");
+    SOLD("sold"),
+    WRONG_STATUS("wrong status !!! !$$#4");
 
     private final String status;
 
