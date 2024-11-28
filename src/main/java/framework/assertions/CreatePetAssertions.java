518419c03a2bd29ba1a6e3b8f05d1b9d4afb5131
4aka <7nuclear@gmail.com>
2024-11-28 12:02:34 +0200

    Update assertions with data types

--- src/main/java/framework/assertions/CreatePetAssertions.java
+++ src/main/java/framework/assertions/CreatePetAssertions.java
@@ -5,16 +5,17 @@ import models.pet.requestModel.PetCreateRequest;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.testng.Assert.assertEquals;
 
 public class CreatePetAssertions {
 
         public static void assertAllFields(PetCreateRequest petModel, PetCreateResponse petResponse) {
-            assertThat(petModel.getId(), equalTo(petResponse.getId()));
-            assertThat(petModel.getCategory().getId(), equalTo(petResponse.getCategory().getId()));
+            assertEquals(petModel.getId(), petResponse.getId());
+            assertEquals(petModel.getCategory().getId(), petResponse.getCategory().getId());
             assertThat(petModel.getCategory().getName(), equalTo(petResponse.getCategory().getName()));
             assertThat(petModel.getName(), equalTo(petResponse.getName()));
             assertThat(petModel.getPhotoUrls(), equalTo(petResponse.getPhotoUrls()));
-            assertThat(petModel.getTags().get(0).getId(), equalTo(petResponse.getTags().get(0).getId()));
+            assertEquals(petModel.getTags().get(0).getId(), petResponse.getTags().get(0).getId());
             assertThat(petModel.getTags().get(0).getName(), equalTo(petResponse.getTags().get(0).getName()));
             assertThat(petModel.getStatus(), equalTo(petResponse.getStatus()));
         }
